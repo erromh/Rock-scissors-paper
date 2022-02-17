d41d883259524c5f510c32550a315dcd013d7e71
erromh <80193366+erromh@users.noreply.github.com>
2022-02-17 17:04:22 +0600

    Update Program.cs

--- core_/Program.cs
+++ core_/Program.cs
@@ -7,6 +7,8 @@ namespace Game
         static void Main(string[] args)
         {
             char stop;
+            int player_win = 0, PC_win = 0, draw = 0;
+            string PC_Throw = Get_RandomNumber();
 
             Console.WriteLine("Rock - scissors - paper \n");
 
@@ -19,21 +21,49 @@ namespace Game
                 Console.WriteLine();
 
                 Console.Write(" -> ");
-                string choise = Console.ReadLine();
+                string Player_Throw = Console.ReadLine();
                 Console.WriteLine();
 
-                Winner(choise);
+                if ((Player_Throw == "0" && PC_Throw == "2") || (Player_Throw == "1" && PC_Throw == "0") || (Player_Throw == "2" && PC_Throw == "1"))
+                {
+                    Console.WriteLine("|-----------|");
+                    Console.WriteLine("| You win ! |");
+                    Console.WriteLine("|-----------|");
+                    Console.WriteLine();
+                    player_win++;
+                }
+
+                else if ((PC_Throw == "0" && Player_Throw == "2") || (PC_Throw == "1" && Player_Throw == "0") || (PC_Throw == "2" && Player_Throw == "1"))
+                {
+                    Console.WriteLine("|----------|");
+                    Console.WriteLine("| PC win ! |");
+                    Console.WriteLine("|----------|");
+                    Console.WriteLine();
+                    PC_win++;
+                }
+
+                else if (Player_Throw == PC_Throw)
+                {
+                    Console.WriteLine("|--------|");
+                    Console.WriteLine("| Draw ! |");
+                    Console.WriteLine("|--------|");
+                    Console.WriteLine();
+                    draw++;
+                }
+
                 Console.WriteLine();
-                Console.WriteLine("Do you wanna play again ? \n\n" + "Press 'y' to continue \nPress 'Ctrl + c' to stop");
+                Console.WriteLine("Do you wanna play again ? \n\n" + "Press 'y' to continue \nPress 'n' to stop");
 
                 Console.WriteLine();
                 stop = Convert.ToChar(Console.ReadLine());
                 Console.WriteLine();
 
-            } while (stop == 'y');
-        }
+                Console.WriteLine("Score : \nPlayer - " + player_win + "\n" + "PC - " + PC_win + "\n" + "Draw - " + draw + "\n");
 
-        static string Generate_Random()
+            } while (stop == 'y');
+
+        }
+        static string Get_RandomNumber()
         {
             string[] player_value = { "0", "1", "2" };
 
@@ -42,49 +72,8 @@ namespace Game
 
             string randThrow = player_value[index];
             return randThrow;
-        }
+        } 
 
-        static string Winner(string player_1)
-        {
-
-            string Player_Throw = player_1;
-            string PC_Throw = Generate_Random();
-
-            int player_win = 0, PC_win = 0;
-
-            if ((Player_Throw == "0" && PC_Throw == "2") || (Player_Throw == "1" && PC_Throw == "0") || (Player_Throw == "2" && PC_Throw == "1"))
-            {   
-                player_win++;
-                Console.WriteLine("|-----------|");
-                Console.WriteLine("| You win ! |" + "\t\t\t Score:\t Player - " + player_win);
-                Console.WriteLine("|-----------|" + "\t\t\t\t PC - " + PC_win);
-                Console.WriteLine();
-                return "Player 1";
-            }
-
-            else if ((PC_Throw == "0" && Player_Throw == "2") || (PC_Throw == "1" && Player_Throw == "0") || (PC_Throw == "2" && Player_Throw == "1"))
-            {   
-                PC_win++;
-                Console.WriteLine("|----------|");
-                Console.WriteLine("| PC win ! |" + "\t\t\t Score:\t Player - " + player_win);
-                Console.WriteLine("|----------|" + "\t\t\t\t PC - " + PC_win);
-                Console.WriteLine();
-                return "Computer";
-            }
-
-            else if (Player_Throw == PC_Throw)
-            {
-                Console.WriteLine("|--------|");
-                Console.WriteLine("| draw ! |" + "\t\t\t Score:\t Player - " + player_win);
-                Console.WriteLine("|--------|" + "\t\t\t\t PC - " + PC_win);
-                Console.WriteLine();
-                return "Draw";
-            }
-
-            else
-            {
-                return "Error";
-            }
-        }
     }
+
 }
